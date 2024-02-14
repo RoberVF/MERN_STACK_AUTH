dcaeedb00317e2fddfd5ca4177e3795c1f3c3ab2
Roberto Morales <robertomoralesfumero@gmail.com>
2024-02-14 00:21:27 +0000

    Creacion de Task con model, controller y funcionalidad del CRUD

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,19 @@
 import {Router} from 'express'
 import {authRequired} from '../middlewares/validateToken.js'
+import {getTasks, getTask, createTask, updateTask, deleteTask} from '../controllers/task.controller.js'
 
 const router = Router()
 
-router.get('/task', authRequired, (req, res) => {
-    res.send('tasks')
-})
+router.get('/tasks', authRequired, getTasks)
+
+router.get('/tasks/:id', authRequired, getTask)
+
+router.post('/tasks', authRequired, createTask)
+
+router.delete('/tasks/:id', authRequired, deleteTask)
+
+router.put('/tasks/:id', authRequired, updateTask)
+
+
 
 export default router

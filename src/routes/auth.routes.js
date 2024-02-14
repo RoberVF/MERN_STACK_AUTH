29e34ac51a69d1c30b1186881831c81b3b2fed35
Roberto Morales <robertomoralesfumero@gmail.com>
2024-02-14 01:21:58 +0000

    Validaciones de datos con zod en register, login y el crud de tasks

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
-import {Router} from 'express'
-import {register, login, logout, profile} from '../controllers/auth.controller.js'
-import {authRequired} from '../middlewares/validateToken.js'
+import { Router } from 'express'
+import { register, login, logout, profile } from '../controllers/auth.controller.js'
+import { authRequired } from '../middlewares/validateToken.middleware.js'
+import { validateSchema } from '../middlewares/validator.middelware.js'
+import { registerSchema, loginSchema } from '../schemas/auth.schema.js'
 
 const router = Router()
 
 
-router.post('/register', register)
+router.post('/register', validateSchema(registerSchema), register)
 
-router.post('/login', login)
+router.post('/login', validateSchema(loginSchema), login)
 
 router.post('/logout', logout)
 
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,6 +1,8 @@
-import {Router} from 'express'
-import {authRequired} from '../middlewares/validateToken.js'
-import {getTasks, getTask, createTask, updateTask, deleteTask} from '../controllers/task.controller.js'
+import { Router } from 'express'
+import { authRequired } from '../middlewares/validateToken.middleware.js'
+import { getTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/task.controller.js'
+import { validateSchema } from '../middlewares/validator.middelware.js'
+import { createTaskSchema } from '../schemas/task.schema.js'
 
 const router = Router()
 
@@ -8,7 +10,7 @@ router.get('/tasks', authRequired, getTasks)
 
 router.get('/tasks/:id', authRequired, getTask)
 
-router.post('/tasks', authRequired, createTask)
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)
 
 router.delete('/tasks/:id', authRequired, deleteTask)
 

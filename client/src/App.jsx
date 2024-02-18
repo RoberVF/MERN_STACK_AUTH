fd0d8084f103f45f2faae50dbde6f5319339d59e
Roberto Morales <robertomoralesfumero@gmail.com>
2024-02-18 12:44:54 +0000

    Configuracion base para frontend con React y Vite, rutas frontend con react-router-dom

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/tasks' element={<h1>Tasks Page</h1>} />
+        <Route path='/add-task' element={<h1>New Task</h1>} />
+        <Route path='/tasks/:id' element={<h1>Update Task</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

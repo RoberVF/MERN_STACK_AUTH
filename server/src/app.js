9f3efa528b2fe10d01a5d9897ee13411cd1f5e82
Roberto Morales <robertomoralesfumero@gmail.com>
2024-03-03 20:47:10 +0000

    CORS

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(cors({
     // origin: FRONTEND_URL,
     AcceseControlAllowOrigin: '*',
-    origin: '*',
+    origin: 'https://task-manager-frontend-r9ow.onrender.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE'
 })) //Permitir que las peticiones del frontend, que estan en otro dominio, se puedan comunicar con el backend
@@ -27,6 +27,7 @@ app.use(cors({
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.urlencoded({extended:false}))
 
 app.use("/api", authRoutes)
 app.use("/api", taskRoutes)

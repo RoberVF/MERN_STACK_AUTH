c6304528cf1a054ee1ffa56a5c1a1f4d4bb45fdf
Roberto Morales <robertomoralesfumero@gmail.com>
2024-03-03 21:17:07 +0000

    COOKIES

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -26,13 +26,11 @@ export const register = async (req, res) => {
             id: userSaved._id
         })
 
-        const cookieOptions = {
+        res.cookie('token', token, {
             sameSite : 'None',
             secure : true,
-            // secure : process.env.NODE_ENV === 'production'
-        }
-
-        res.cookie('token', token, cookieOptions)
+            httpOnly: false
+        })
         res.json({
             id: userSaved._id,
             username: userSaved.username,

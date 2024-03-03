e1ef0ad348c5dc4b0729de84a23b66eb11fe2550
Roberto Morales <robertomoralesfumero@gmail.com>
2024-03-03 20:47:23 +0000

    CORS

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,6 @@ const app = express()
 
 app.use(cors({
     // origin: FRONTEND_URL,
-    AcceseControlAllowOrigin: '*',
     origin: 'https://task-manager-frontend-r9ow.onrender.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE'

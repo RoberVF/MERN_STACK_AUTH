083fe18ae74aed0fb04b2226e342c7120d6b2685
Roberto Morales <robertomoralesfumero@gmail.com>
2024-03-17 14:43:54 +0000

    Arreglo de politicas CORS, funcionando correctamente

--- server/src/app.js
+++ server/src/app.js
@@ -6,27 +6,33 @@ import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 import { FRONTEND_URL } from './config.js'
 
 const app = express()
 
-
-
-app.use(cors({
-    // origin: FRONTEND_URL,
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE'
-})) //Permitir que las peticiones del frontend, que estan en otro dominio, se puedan comunicar con el backend
-
-
-
-
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
+
+//CORS
+const corsOptions = {
+    // origin: FRONTEND_URL || 'http://localhost:5173',
+    origin: function(origin, callback){
+        if(!origin || FRONTEND_URL === origin){
+            callback(null, true)
+        } else{
+            callback(new Error("Not allowed by CORS Policity"))
+        }
+    },
+    credentials: true,
+}
+app.use(cors(corsOptions)) //Permitir que las peticiones del frontend, que estan en otro dominio, se puedan comunicar con el backend
+app.options("*", cors())
 
 app.use("/api", authRoutes)
 app.use("/api", taskRoutes)
